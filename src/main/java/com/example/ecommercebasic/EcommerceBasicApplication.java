4e2e79754fbacccb21edb7bb2c4f60c723cb6f78
Dev <165308292+DennyM55@users.noreply.github.com>
2026-02-08 17:11:20 +0530

    git init

---
+++ src/main/java/com/example/ecommercebasic/EcommerceBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBasicApplication.class, args);
+    }
+
+}
